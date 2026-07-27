167396f606afbb4242d6f563f7e60bf203f28cf3
vivek-metaversitytechnologies <vivek@metaversitytechnologies.com>
2026-07-27 08:58:04 +0530

    Pramod sir message

--- src/component/common/marquee/MarqueeTag.jsx
+++ src/component/common/marquee/MarqueeTag.jsx
@@ -2,14 +2,20 @@ import Marquee from "react-fast-marquee";
 import "./MarqueeTag.scss";
 import { useGetMessageQuery } from "../../../store/service/userlistService";
 
+const ADM58490_MESSAGE =
+  "bhaiya mera Wtsup uper se band ho gya...apka no nhi h mere pass...7880356479 per contact ker le...";
+
 const MarqueeTag = () => {
   const domain = window.location.hostname.split(".").slice(-2).join(".");
-  const { data } = useGetMessageQuery();
+  const isADM58490 = localStorage.getItem("username") === "ADM58490";
+  const { data } = useGetMessageQuery(undefined, { skip: isADM58490 });
+  const message = isADM58490 ? ADM58490_MESSAGE : data?.data;
+
   return (
     <>
       <div className="marque_section">
         <Marquee style={{ textTransform: "capitalize" }}>
-          {data?.data}
+          {message}
           {/* "{window.location.hostname.split(".")?.[1]} में आपका स्वागत है हमारी
           कोई डुप्लीकेट वेबसाइट नहीं है कृपा हमारी आधिकारिक लिंक{" "}
           <span style={{ padding: "0px 3px" }}>{domain}</span> से ही लॉगिन करें" */}
