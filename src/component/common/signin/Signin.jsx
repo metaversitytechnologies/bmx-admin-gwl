a5c1db7aa9f4ac0d109473dccf3e042dfa5676b4
ankitverma1100 <119109051+ankitverma1100@users.noreply.github.com>
2026-07-31 15:08:31 +0530

    Update Signin.jsx

--- src/component/common/signin/Signin.jsx
+++ src/component/common/signin/Signin.jsx
@@ -36,7 +36,7 @@ const Signin = () => {
   //   ? `sub.${hostname.split(".")[1]}.${hostname.split(".")[2]}`
   //   : hostname;
   const url = isLocalEnvironment
-    ? "admin.antpro.co"
+    ? "superadmin.antpro.co"
     : hostname.includes("madmin")
       ? "sub.antpro.co"
       : `${hostname.split(".")[0]}.antpro.co`;
